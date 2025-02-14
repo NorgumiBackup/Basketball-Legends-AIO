04f47326585f5780e3ca5055d0915317162af12b
NorgumiBackup <erocgreen2006@gmail.com>
2025-02-14 05:56:04 -0600

    Create lua.lua

---
+++ lua.lua
@@ -0,0 +1,4 @@
+-- GitHub: https://github.com/USSIndustry
+-- Notice: If you see my scripts being sold, that's not me. All my scripts are free and can be found on my GitHub page. I try to update weekly. If ball mag doesn’t work, DM me—it's just some old script I had in my drive since last year and just added it to the UI.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/USSIndustry/BL-Storage/refs/heads/main/zeze.lua"))()
